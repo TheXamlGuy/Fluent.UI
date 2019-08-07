9ca92522885c89d7fe46d83be4700d82725a1a12
Daniel <dan_clark@outlook.com>
2019-08-07 22:57:52 +0100

    wip

--- Fluent.UI.Controls/DependencyPropertyObserver.cs
+++ Fluent.UI.Controls/DependencyPropertyObserver.cs
@@ -1,11 +1,32 @@
 ﻿using System;
 using System.Windows;
+using System.Windows.Data;
 
 namespace Fluent.UI.Controls
 {
-    public sealed class DependencyPropertyObserver : DependencyObject, IDisposable
+    public class DependencyPropertyObserver : DependencyObject, IDisposable
     {
         private readonly WeakReference _propertyReference;
+
+        private DependencyPropertyObserver(DependencyObject propertySource, PropertyPath propertyPath)
+        {
+            _propertyReference = new WeakReference(propertySource);
+
+            var propertyBinding = new Binding
+            {
+                Path = propertyPath,
+                Mode = BindingMode.OneWay,
+                Source = propertySource
+            };
+
+            BindingOperations.SetBinding(this, ValueProperty, propertyBinding);
+        }
+
+        public static DependencyPropertyObserver Register(DependencyObject dependencyObject, PropertyPath propertyPath)
+        {
+            return new DependencyPropertyObserver(dependencyObject, propertyPath);
+        }
+
         public void Dispose()
         {
             throw new NotImplementedException();
