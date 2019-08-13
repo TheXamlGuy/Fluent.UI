96a0c1c7d293422a13c94b1ff9f3d200d753d7e3
Daniel Clark <dan_clark@outlook.com>
2019-08-13 17:50:15 +0100

    Moved to .net core

--- Fluent.UI.Controls/Properties/AssemblyInfo.cs
+++ Fluent.UI.Controls/Properties/AssemblyInfo.cs
@@ -1,18 +1,7 @@
-﻿using System.Reflection;
 using System.Runtime.InteropServices;
 using System.Windows;
 using System.Windows.Markup;
 
-[assembly: AssemblyTitle("Fluent.UI.Controls")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Fluent.UI.Controls")]
-[assembly: AssemblyCopyright("Copyright ©  2019")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
 [assembly: ThemeInfo(ResourceDictionaryLocation.None, ResourceDictionaryLocation.SourceAssembly)]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
 [assembly: XmlnsDefinition("http://schemas.microsoft.com/winfx/2006/xaml/presentation", "Fluent.UI.Controls")]
