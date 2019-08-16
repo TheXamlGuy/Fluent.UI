6939317bec63310948b024ecd4bd04a28c9304ff
Daniel Clark <Daniel.Clark@kromek.com>
2019-08-16 13:32:46 +0100

    Tidy up

--- Fluent.UI.Demo/MainWindow.xaml.cs
+++ Fluent.UI.Demo/MainWindow.xaml.cs
@@ -1,16 +1,6 @@
 ﻿using Fluent.UI.Controls;
-using Fluent.UI.Core.Extensions;
-using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.Collections.ObjectModel;
-using System.IO;
-using System.Linq;
-using System.Reflection;
 using System.Windows;
-using System.Windows.Controls;
 using System.Windows.Media;
-using System.Windows.Media.Animation;
 
 namespace Fluent.UI.Demo
 {
@@ -19,42 +9,18 @@ namespace Fluent.UI.Demo
         public MainWindow()
         {
             InitializeComponent();
-
-            Button2.Click += Button2_Click;
         }
 
-        private void Button_Click(object sender, RoutedEventArgs e)
+        private void CheckBox1_Checked(object sender, RoutedEventArgs e)
         {
-            var contentDialog = new ContentDialog();
-            contentDialog.PrimaryButtonText = "sf";
-            contentDialog.ShowAsync();
+            Border.Background = Brushes.Black;
+            ButtonExtension.SetRequestedTheme(Button, ElementTheme.Dark);
         }
 
-        private void Button2_Click(object sender, RoutedEventArgs e)
+        private void CheckBox1_Unchecked(object sender, RoutedEventArgs e)
         {
-            ContentDialogXAML.ShowAsync();
-        }
-
-        private void D_Checked(object sender, RoutedEventArgs e)
-        {
-            d.Content = "Switch to dark theme";
-
-            foreach (var d in ThemeBackground.Children)
-            {
-                ButtonExtension.SetRequestedTheme(d as Button, ElementTheme.Dark);
-            }
-            ThemeBackground.Background = Brushes.Black;
-
-        }
-
-        private void D_Unchecked(object sender, RoutedEventArgs e)
-        {
-            d.Content = "Switch to light theme";
-            foreach (var d in ThemeBackground.Children)
-            {
-                ButtonExtension.SetRequestedTheme(d as Button, ElementTheme.Light);
-            }
-            ThemeBackground.Background = Brushes.Transparent;
+            Border.Background = Brushes.Transparent;
+            ButtonExtension.SetRequestedTheme(Button, ElementTheme.Light);
         }
     }
 }
