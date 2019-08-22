c3a165efc42d7975723253bac18c6f9a7806d85a
Daniel <dan_clark@outlook.com>
2019-08-22 06:51:25 +0100

    resdesign demo app

--- Fluent.UI.Demo/MainWindow.xaml.cs
+++ Fluent.UI.Demo/MainWindow.xaml.cs
@@ -14,21 +14,21 @@ namespace Fluent.UI.Demo
 
         private void CheckBox1_Checked(object sender, RoutedEventArgs e)
         {
-            CheckBox1.Content = "Switch to dark theme";
-            FrameworkElementExtension.SetRequestedTheme(Border1, ElementTheme.Dark);
-            Border1.Background = Brushes.Black;
+            //CheckBox1.Content = "Switch to dark theme";
+            //FrameworkElementExtension.SetRequestedTheme(WrapPanel, ElementTheme.Dark);
+            //Border1.Background = Brushes.Black;
 
-            FrameworkElementExtension.SetRequestedTheme(Border2, ElementTheme.Dark);
-            Border2.Background = Brushes.Black;
+            //FrameworkElementExtension.SetRequestedTheme(Border2, ElementTheme.Dark);
+            //Border2.Background = Brushes.Black;
         }
 
         private void CheckBox1_Unchecked(object sender, RoutedEventArgs e)
         {
-            CheckBox1.Content = "Switch to light theme";
-            FrameworkElementExtension.SetRequestedTheme(Border1, ElementTheme.Light);
-            Border1.Background = Brushes.Transparent;
-            FrameworkElementExtension.SetRequestedTheme(Border2, ElementTheme.Light);
-            Border2.Background = Brushes.Transparent;
+            //CheckBox1.Content = "Switch to light theme";
+            //FrameworkElementExtension.SetRequestedTheme(Border1, ElementTheme.Light);
+            //Border1.Background = Brushes.Transparent;
+            //FrameworkElementExtension.SetRequestedTheme(Border2, ElementTheme.Light);
+            //Border2.Background = Brushes.Transparent;
         }
     }
 }
