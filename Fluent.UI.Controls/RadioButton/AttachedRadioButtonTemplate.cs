faef1496f30eb56c0321a031837f3604d46f0cf4
Daniel <dan_clark@outlook.com>
2019-08-26 10:18:05 +0100

    WIP

---
+++ Fluent.UI.Controls/RadioButton/AttachedRadioButtonTemplate.cs
@@ -0,0 +1,11 @@
+﻿using Fluent.UI.Core;
+using System.Windows.Controls;
+
+namespace Fluent.UI.Controls
+{
+    //[DefaultStyleTarget(typeof(RadioButton))]
+    //public class AttachedRadioButtonTemplate : AttachedControlTemplate<RadioButton>
+    //{
+
+    //}
+}
